f4d35a4092ba4a3ba037332e65391b947d6a104f
Suyash Tiwary <123478283+Suyashtiwary12@users.noreply.github.com>
2024-07-11 00:54:56 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const model = require("./models/transactionModel")
-// const database = require("./config/database");
+const database = require("./config/database");
 const dotenv= require("dotenv").config();
 
 app.use(cors());
