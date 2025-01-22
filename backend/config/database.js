69729dd97361ea66be440ed7d2f1230c4900b665
Suyash Tiwary <Suyash.tonystark@gmail.com>
2025-01-22 16:16:50 +0530

    changes

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,10 +1,11 @@
-const mongoose=require("mongoose");
-const dotenv = require("dotenv").config();
-const url= process.env.CONNECT_URI;
+const mongoose= require("mongoose");
+require("dotenv").config();
+const url = process.env.CONNECT_URL;
+console.log(url);
 
 
-const database=()=>{
-    mongoose.connect(url,{
+const database = () => {
+    mongoose.connect("mongodb+srv://suyashtonystark:suyashtonystark12@cluster0.yhoqsps.mongodb.net/spendwise",{
         useNewUrlParser: true,
         useUnifiedTopology: true,
     }).then(()=>{
